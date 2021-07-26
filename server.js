87af954dd99cdde21b73b6398b7cc38dae877b4d
Rachel Solov <rsolov23@yahoo.com>
2021-07-26 17:51:52 -0500

    deploying

--- server.js
+++ server.js
@@ -16,14 +16,19 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker",
-  {
-    useFindAndModify: false,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://localhost/Budget-Tracker",
+//   {
+//     useFindAndModify: false,
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 // routes
 app.use(require("./routes/api.js"));
